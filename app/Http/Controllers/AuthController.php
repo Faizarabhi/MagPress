d3a30fc7e3c9424fe7dbe5e17ac1782a5ab147d9
Faiza RABHI <93930498+Faizarabhi@users.noreply.github.com>
2025-07-15 02:13:06 +0100

    feat: add login function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -40,6 +40,30 @@ class AuthController extends Controller
         }
 
 
+}
 
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'Unauthorized',
+            ])->setStatusCode(401, 'Unauthorized');
+        }
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'Login successful',
+            'user' => $user,
+            'authorization' => [
+                'token' => $token,
+                'type' => 'Bearer',
+            ],
+        ])->setStatusCode(200, 'OK');
     }
 }
